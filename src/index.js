67442a787839cccfed4754444ad3fc0727c29a0a
Dmitry Shaido <d.shaido@gmail.com>
2018-02-26 15:44:18 +0300

    task 3

--- src/index.js
+++ src/index.js
@@ -1,30 +1,57 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.elements = new Array();
   }
 
   add(element) {
-    // your implementation
+    this.elements.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.elements[index];
   }
 
   get length() {
-    // your implementation
+    return this.elements.length;
   }
 
   toArray() {
-    // your implementation
+    return this.elements;
   }
 
   sort(indices) {
-    // your implementation
-  }
+    for (var i = (indices.length -1); i >= 0; i --) {
+      for (var j = (indices.length - 1); j > 0; j --) {
+        if (indices[j] < indices[j - 1]) {
+          var temp = indices[j];
+          indices[j] = indices[j - 1];
+          indices[j - 1] = temp; 
+        }
+      }
+    }
+
+    if (this.comparator === undefined) {
+      for (var i = 0; i < indices.length; i++) { 
+        for (var j = 0; j < indices.length; j++) { 
+          if (this.elements[indices[i]] < this.elements[indices[j]]) { 
+            var temp = this.elements[indices[i]];
+            this.elements[indices[i]] = this.elements[indices[j]];
+            this.elements[indices[j]] = temp;
+          }
+        }
+      }  
+    } else {
+            if (this.comparator(this.elements[indices[0]], this.elements[indices[1]]) > 0) {
+              var temp = this.elements[indices[0]];
+              this.elements[indices[0]] = this.elements[indices[1]];
+              this.elements[indices[1]] = temp;
+            }     
+    }        
+}
+    
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;
   }
 }
 
